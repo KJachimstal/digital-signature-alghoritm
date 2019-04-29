730feee16cf45a7678b008ec70b524bf784ed6c1
KJachimstal <k,jachimstal@gmail.com>
2019-04-29 21:13:41 +0200

    Add tests for KeyGenerator class

--- src/test/java/dsa/KeyGeneratorTest.java
+++ src/test/java/dsa/KeyGeneratorTest.java
@@ -1,5 +1,6 @@
 package dsa;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.math.BigInteger;
@@ -8,9 +9,37 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class KeyGeneratorTest {
 
+    static KeyGenerator key;
+
+    @BeforeAll
+    static void initialize() {
+        key = new KeyGenerator(512);
+        BigInteger p, h, q, b, a;
+        key.generate();
+    }
+
     @Test
-    void generateBigInteger() {
-        BigInteger result = KeyGenerator.generatePrimeNumber(64);
-        assertEquals(64, result.bitLength() );
+    void generatePrimeNumber() {
+        assertEquals(512, key.getP().bitLength());
+    }
+
+    @Test
+    void generateQ() {
+        assertEquals(160, key.getQ().bitLength());
+    }
+
+    @Test
+    void generateH() {
+        assertTrue(key.getH().compareTo(key.getP()) == -1);
+    }
+
+    @Test
+    void generateA() {
+        assertTrue(key.getA().compareTo(key.getQ()) == -1);
+    }
+
+    @Test
+    void generateB() {
+        assertTrue(key.getB().compareTo(key.getH().modPow(key.getA(), key.getP())) == 0);
     }
 }
