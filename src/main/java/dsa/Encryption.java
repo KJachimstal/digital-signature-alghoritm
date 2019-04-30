aa4c9c2b6b90efc310cd557ab6efeb03ee5df18a
KJachimstal <k,jachimstal@gmail.com>
2019-04-30 12:36:22 +0200

    Add generateR method on fill constructor of Encryption class

--- src/main/java/dsa/Encryption.java
+++ src/main/java/dsa/Encryption.java
@@ -2,14 +2,28 @@ package dsa;
 
 import dsa.keys.PublicKey;
 
-public class Encryption extends Cryptography {
-    private PublicKey publicKey;
+import java.math.BigInteger;
+import java.util.Random;
 
-    public Encryption() {
-        // TODO: Implement encryption constructor
+public class Encryption extends Cryptography {
+    private KeyGenerator keyGenerator;
+    private BigInteger r, s1, s2;
+
+    public Encryption(int length) {
+        keyGenerator = new KeyGenerator(length);
+        r = generateR(keyGenerator.getQ());
     }
 
     public void encrypt() {
-        // TODO: Implement encryption method
+    }
+
+    public BigInteger generateR(BigInteger q) {
+        Random rng = new Random();
+        int range = 1 + rng.nextInt(keyGenerator.getQ().subtract(BigInteger.ONE).toString().length());
+        BigInteger result = new BigInteger(range, rng);
+
+        result = result.modInverse(q);
+
+        return result;
     }
 }
