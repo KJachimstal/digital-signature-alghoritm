d6f143c006296d8e14d007dd96fa37a5b63c0936
KJachimstal <k,jachimstal@gmail.com>
2019-04-30 22:03:11 +0200

    Add block class and fill operations

---
+++ src/main/java/dsa/Block.java
@@ -0,0 +1,54 @@
+package dsa;
+
+import java.math.BigInteger;
+import java.util.Arrays;
+
+public class Block {
+    private byte[] data;
+    public int fill = -1;
+
+    public Block(byte[] data) {
+        this.data = data.clone();
+    }
+
+    public Block(BigInteger number, int fill) {
+        this(Operations.fillArray(number.toByteArray(), fill));
+        this.fill = fill;
+    }
+
+    public BigInteger getNumber() {
+        System.out.println("GET NUMBER: " + Arrays.toString(data) + " / " + data.length);
+        if (data[0] < 0) {
+            byte[] tmp = new byte[data.length + 1];
+            tmp[0] = 0;
+            System.arraycopy(data, 0, tmp, 1, data.length);
+            data = tmp;
+        }
+        return new BigInteger(data);
+    }
+
+    public byte[] getData() {
+        System.out.println("GET DATA: " + Arrays.toString(data) + " / " + data.length + " / " + fill);
+        if (data.length > 1 && data[0] == 0 && data[1] < 0) {
+            byte[] tmp = new byte[data.length - 1];
+            System.out.println(data.length + " REDUCED TO " + tmp.length);
+            System.arraycopy(data, 1, tmp, 0, data.length - 1);
+            return tmp;
+        }
+        return data;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < data.length; i++) {
+            sb.append(data[i]);
+            if (i != data.length - 1) {
+                sb.append(",");
+            }
+        }
+        sb.append("]");
+        return sb.toString();
+    }
+}
