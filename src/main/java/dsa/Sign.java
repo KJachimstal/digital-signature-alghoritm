7bfcc5ef50016e5233b647d0ffdc35e1ad3e6cc5
KJachimstal <k,jachimstal@gmail.com>
2019-05-01 18:38:07 +0200

    Correction all methods in Sign class

--- src/main/java/dsa/Sign.java
+++ src/main/java/dsa/Sign.java
@@ -7,34 +7,46 @@ import java.util.Random;
 
 public class Sign extends Cryptography {
     private PrivateKey privateKey;
-    private BigInteger r, s1, s2;
+    private BigInteger r, rPrim, s1, s2;
     private Block[] results;
     private Block[] data;
 
     public Sign(Block[] data, PrivateKey privateKey) {
-        r = generateR(privateKey.getQ());
         this.data = data;
         this.privateKey = privateKey;
+        r = generateR();
+        rPrim = generateRPrim();
     }
 
     public void encrypt() {
         results = new Block[data.length * 2];
         for (int i = 0; i < data.length; i++) {
-            BigInteger m = new BigInteger(data[i].getData());
+            BigInteger m = data[i].getBigInteger();
+
             s1 = privateKey.getH().modPow(r, privateKey.getP()).mod(privateKey.getQ());
-            s2 = r.multiply(m.add(privateKey.getA().multiply(s1))).mod(privateKey.getQ());
+            s2 = rPrim.multiply(m.add(privateKey.getA().multiply(s1))).mod(privateKey.getQ());
 
             results[i * 2] = new Block(s1, privateKey.getFillSize());
             results[i * 2 + 1] = new Block(s2, privateKey.getFillSize());
         }
     }
 
-    public BigInteger generateR(BigInteger q) {
+    public BigInteger generateR() {
         Random rng = new Random();
-        int range = 1 + rng.nextInt(privateKey.getQ().subtract(BigInteger.ONE).toString().length());
-        BigInteger result = new BigInteger(range, rng);
+        BigInteger q = privateKey.getQ();
+        BigInteger result;
+        int range = 1 + rng.nextInt(q.bitLength() - 2);
+        do {
+            result = new BigInteger(range, rng);
+        } while (result.equals(BigInteger.ZERO));
 
-        return result.modInverse(q);
+        return result;
+    }
+
+    public BigInteger generateRPrim() {
+        System.out.println("R: "+r);
+        System.out.println("Q: "+privateKey.getQ());
+        return rPrim = r.modInverse(privateKey.getQ());
     }
 
     public Block[] getResults() {
