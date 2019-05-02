fc203172f1f691af861462883f78f1216e5fbb3a
KJachimstal <k,jachimstal@gmail.com>
2019-05-02 11:08:42 +0200

    Add pow method to Operations class

--- src/main/java/dsa/Operations.java
+++ src/main/java/dsa/Operations.java
@@ -79,6 +79,15 @@ public class Operations {
         }catch (NoSuchAlgorithmException ex){
             return null;
         }
+    }
 
+    public static BigInteger pow(BigInteger base, BigInteger exponent) {
+        BigInteger result = BigInteger.ONE;
+        while (exponent.signum() > 0) {
+            if (exponent.testBit(0)) result = result.multiply(base);
+            base = base.multiply(base);
+            exponent = exponent.shiftRight(1);
+        }
+        return result;
     }
 }
